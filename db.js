9a4067e8a3b03bcfa7589f664d3c6724ffac7c90
RohanKansara01 <rohankansara0110@gmail.com>
2024-05-09 19:36:33 +0530

    app creation

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+const connection=mongoose.connect(`mongodb+srv://rohankansara2000:test@databasetest.cehhvv0.mongodb.net/FoodDeliveryApp`);
+
+
+
+
+
+
+
+
+module.exports={connection};
